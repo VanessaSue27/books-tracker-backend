a8d6dbb4079a7ef2487e2ae13b8676d2baeae48e
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2021-03-02 13:38:49 +0100

    Initial commit: base setup done

--- server.js
+++ server.js
@@ -1,24 +1,80 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cloudinaryFramework from 'cloudinary';
+import multer from 'multer';
+import cloudinaryStorage from 'multer-storage-cloudinary';
+import mongoose from 'mongoose';
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+// Set up MongoDB
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/books-tracker';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+// Book Model
+const Book = mongoose.model('Book', {
+  name: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  }
+});
+
+dotenv.config();
+
+// Cloudinary Setup
+const cloudinary = cloudinaryFramework.v2; 
+cloudinary.config({
+  cloud_name: 'vanessa-cloudinary',
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'imageupload-practice',
+    allowedFormats: ['jpg', 'jpeg', 'png'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }],
+  }
+});
+
+const parser = multer({ storage });
+
+// Defines the port the app will run on
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Vanessas app to track which books I have read so far...');
+});
+
+// Endpoint to create a new Book instance: Including book name, author and image
+// In the Frontend, we should send in the body: name, author and attached image file
+app.post('/books', parser.single('image'), async (req, res) => {
+  try {
+    const newBook = await new Book({ name: req.body.name, author: req.body.author, imageUrl: req.file.path }).save();
+    // Maybe not send whole newBook object back? Instead a success message
+    res.status(200).json(newBook);
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save New Book to the database', error });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
